67cf525b1c01a735aa8ed7b4f111003cbafceafd
Drbobby27 <envioshseq@gmail.com>
2022-09-13 22:08:54 -0500

    fin

--- app.js
+++ app.js
@@ -15,35 +15,35 @@ const app = new Vue({
         calculoInventario: "",
         alerta:"",
         errors: false
-
     },
     methods: {
-        
         agregar(){
-            
-            this.dataBodegas.push(
-                {
-                    bodega: this.defaultBodega ,
-                    cantidad: this.cantidad,
-                    tipo:this.defaultPeso,
-                    conversion: this.conversionValues()
-                }
-            )
-            this.acumulador()
-            
-            console.log(this.dataBodegas)
-            console.log(`[conversion]: ${this.conversionValues()}`)
-
-            this.limpiar()   
-           
+            if(this.cantidad === "" || this.cantidad < 1 || this.defaultBodega === null  || this.defaultPeso === null)  {
+                this.errors = true;
+                // alert(this.errors)
+            } else {
+                this.dataBodegas.push(
+                    {
+                        bodega: this.defaultBodega ,
+                        cantidad: this.cantidad,
+                        tipo:this.defaultPeso,
+                        conversion: this.conversionValues()
+                    }
+                )
+                this.acumulador()
+                console.log(this.dataBodegas)
+                console.log(`[conversion]: ${this.conversionValues()}`)
+                this.limpiar()
+                this.mensaje();
+            }
         },
         acumulador(){
           if(this.defaultBodega === 'Bodega1') {
                 this.acumuladoBodega1 += this.conversionValues()
           } else{
-                this.acumuladoBodega2 += this.conversionValues()
+                this.acumuladoBodega2 += this.conversionValues()//total kg
           }
-        },        
+        },
         limpiar(){
             this.defaultBodega= null,
             this.defaultPeso= null,
@@ -59,15 +59,58 @@ const app = new Vue({
             else {
                 return this.cantidad
             }
-              
-               
         } ,
-        alert1() {
+        mensaje() {
+            if(this.acumulado1() === 10000) {
+                return this.alertA10();
+            } else if(this.acumulado1() === 50000) {
+                return this.alertA50();
+            }else if(this.acumulado2() === 26000) {
+                return this.alertB10();
+            } else if(this.acumulado2() === 115000) {
+                return this.alertB50();
+            } else {
+                console.log('Estamos en problemas')
+            }
+        },
+        acumulado1() {
+            return this.Bodega1 - this.acumuladoBodega1
+        },
+        acumulado2() {
+            return this.Bodega2 - this.acumuladoBodega2
+        },
+        alertA10() {
             Swal.fire(
-                'Cuidado',
-                'Tu inventario esta en un 10%',
+                'Precaución',
+                'El inventario de la Bodega1 esta en un 10%',
                 'warning'
               )
+        },
+        alertA50() {
+            Swal.fire(
+                'Cuidado',
+                'El inventario de la Bodega1 esta en un 50%',
+                'warning'
+              )
+        },
+        alertB10() {
+            Swal.fire(
+                'Precaución',
+                'El inventario de la Bodega2 esta en un 10%',
+                'warning'
+              )
+        },
+        alertB50() {
+            Swal.fire(
+                'Cuidado',
+                'El inventario de la Bodega2 esta en un 50%',
+                'warning'
+              )
+        },
+        validacionesErrors() {
+            if(this.cantidad === "" || this.cantidad < 1 || this.defaultBodega === null  || this.defaultPeso === null)  {
+                this.errors = true;
+            }
         }
-    }    
+    }
   })
